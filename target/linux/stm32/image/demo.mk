91993753e65354e2296a1ef9dd8ea9631707778c
Thomas Richard <thomas.richard@bootlin.com>
2024-02-22 16:27:09 +0100

    stm32: add some demo devices
    Add some demo devices based on STM32MP157F-DK2 and STM32MP135F-DK devices.
    They also include the Luci web interface.
    
    Signed-off-by: Thomas Richard <thomas.richard@bootlin.com>

---
+++ target/linux/stm32/image/demo.mk
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2024 Bootlin
+#
+
+LUCI := uhttpd luci luci-ssl luci-theme-openwrt-2020
+
+define Device/Demo
+  DEVICE_VARIANT := (demo)
+  DEVICE_PACKAGES += $(LUCI)
+endef
+
+define Device/stm32mp157f-dk2-demo
+  $(call Device/stm32mp157f-dk2)
+  $(call Device/Demo)
+  DEVICE_NAME := stm32mp157f-dk2
+endef
+
+define Device/stm32mp135f-dk-demo
+  $(call Device/stm32mp135f-dk)
+  $(call Device/Demo)
+  DEVICE_NAME := stm32mp135f-dk
+endef
+
+ifeq ($(SUBTARGET),stm32mp1)
+  TARGET_DEVICES += \
+		    stm32mp157f-dk2-demo \
+		    stm32mp135f-dk-demo
+endif
