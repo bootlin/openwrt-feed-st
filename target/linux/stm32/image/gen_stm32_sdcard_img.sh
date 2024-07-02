2b6811bbbaac739f0f2df7a7ae2846fe139607f0
Thomas Richard <thomas.richard@bootlin.com>
2024-07-02 15:16:16 +0200

    stm32: image: remove a useless fsbl partition
    Remove the useless fsbl2 partition, and rename fsbla partition to fsbl1 to
    be compatible with stm32mp1 and stm32mp2.
    
    Signed-off-by: Thomas Richard <thomas.richard@bootlin.com>

--- target/linux/stm32/image/gen_stm32_sdcard_img.sh
+++ target/linux/stm32/image/gen_stm32_sdcard_img.sh
@@ -16,22 +16,19 @@ ENVSIZE="$((${6} / 1024))"
 BOOTFSSIZE="${7}"
 ROOTFSSIZE="${8}"
 
-set $(ptgen -o "${OUTPUT}" -g -a 5 -l 2048 -G ${GUID} -N fsbl1 -p 2M -N fsbl2 -p 2M -N fip -p 2M -N u-boot-env -p "${ENVSIZE}" -N boot -p${BOOTFSSIZE}M -N rootfs -p ${ROOTFSSIZE}M)
-FSBL1OFFSET="$((${1} / 512))"
-FSBL1SIZE="$((${2} / 512))"
-FSBL2OFFSET="$((${3} / 512))"
-FSBL2SIZE="$((${4} / 512))"
-FIPOFFSET="$((${5} / 512))"
-FIPSIZE="$((${6} / 512))"
-ENVOFFSET="$((${7} / 512))"
-ENVSIZE="$((${8} / 512))"
-BOOTFSOFFSET="$((${9} / 512))"
-BOOTFSSIZE="$((${10} / 512))"
-ROOTFSOFFSET="$((${11} / 512))"
-ROOTFSSIZE="$((${12} / 512))"
+set $(ptgen -o "${OUTPUT}" -g -a 4 -l 2048 -G ${GUID} -N fsbla -p 2M -N fip -p 3M -N u-boot-env -p "${ENVSIZE}" -N boot -p${BOOTFSSIZE}M -N rootfs -p ${ROOTFSSIZE}M)
+FSBLAOFFSET="$((${1} / 512))"
+FSBLASIZE="$((${2} / 512))"
+FIPOFFSET="$((${3} / 512))"
+FIPSIZE="$((${4} / 512))"
+ENVOFFSET="$((${5} / 512))"
+ENVSIZE="$((${6} / 512))"
+BOOTFSOFFSET="$((${7} / 512))"
+BOOTFSSIZE="$((${8} / 512))"
+ROOTFSOFFSET="$((${9} / 512))"
+ROOTFSSIZE="$((${10} / 512))"
 
-dd bs=512 if="${FSBL}" of="${OUTPUT}" seek="${FSBL1OFFSET}" conv=notrunc
-dd bs=512 if="${FSBL}" of="${OUTPUT}" seek="${FSBL2OFFSET}" conv=notrunc
+dd bs=512 if="${FSBL}" of="${OUTPUT}" seek="${FSBLAOFFSET}" conv=notrunc
 dd bs=512 if="${FIP}"  of="${OUTPUT}" seek="${FIPOFFSET}" conv=notrunc
 dd bs=512 if=/dev/zero of="${OUTPUT}" seek="${ENVOFFSET}" count="${ENVSIZE}" conv=notrunc
 dd bs=512 if="${BOOTFS}" of="${OUTPUT}" seek="${BOOTFSOFFSET}" conv=notrunc
