58fad57cb21c1df36f2a0281b39baf5ab1b36843
Thomas Richard <thomas.richard@bootlin.com>
2024-12-16 15:57:16 +0100

    stm32: switch to kernel 6.6
    The mp1 kernel config is based on the upstream config, with few
    modifications for STM32MP157F-DK2 support.
    
    The mp2 kernel config was based on ST config with a big cleanup (which can
    be improved).
    
    Side note, CONFIG_REALTEK_PHY is mandatory for STM32MP257F-EV1.
    For mp1 subtarget it works well with generic PHY support, but add full PHY
    support (CONFIG_REALTEK_PHY for STM32MP135F-DK and CONFIG_SMSC_PHY for
    STM32MP157F-DK2), it wont hurt anything.
    
    Signed-off-by: Thomas Richard <thomas.richard@bootlin.com>

--- target/linux/stm32/modules.mk
+++ target/linux/stm32/modules.mk
@@ -5,6 +5,21 @@
 # See /LICENSE for more information.
 #
 
+define KernelPackage/phy-stm32-usbphyc
+  TITLE:=STM32 USB HS PHY Controller driver
+  DEPENDS:=@TARGET_stm32
+  KCONFIG:=CONFIG_PHY_STM32_USBPHYC
+  FILES:=$(LINUX_DIR)/drivers/phy/st/phy-stm32-usbphyc.ko
+  AUTOLOAD:=$(call AutoProbe,phy-stm32-usbphyc,)
+endef
+
+define KernelPackage/phy-stm32-usbphyc/description
+  Kernel module for STM32 USB HS PHY Controller
+endef
+
+$(eval $(call KernelPackage,phy-stm32-usbphyc))
+
+
 define KernelPackage/usb-stm32-usbh
   TITLE:=STM32 USB2 Host Glue support
   KCONFIG = CONFIG_USB_STM32_USBH
